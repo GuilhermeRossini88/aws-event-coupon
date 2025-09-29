d5f968caeba7e9d489bfa7e7328080382f4641f2
guilherme <gui.rossini@hotmail.com>
2025-09-29 17:57:45 -0300

    feat(models): adiciona entidades Coupon e Event

---
+++ src/main/java/com/rtech/eventos/model/Coupon/Coupon.java
@@ -0,0 +1,31 @@
+package com.rtech.eventos.model.Coupon;
+
+import com.rtech.eventos.model.Event.Event;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Table(name = "coupon")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Coupon {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String code;
+    private Integer discount;
+    private Date valid;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+
+}
