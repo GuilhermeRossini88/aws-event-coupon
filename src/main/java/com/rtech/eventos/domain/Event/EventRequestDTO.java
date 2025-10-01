18c9cc4e31ca506318a59171693731fe7aafdf25
guilherme <gui.rossini@hotmail.com>
2025-10-01 11:07:26 -0300

    feat: create DTO

---
+++ src/main/java/com/rtech/eventos/domain/Event/EventRequestDTO.java
@@ -0,0 +1,8 @@
+package com.rtech.eventos.domain.Event;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Date;
+
+public record EventRequestDTO(String title, String description, Long date, String city, String state, Boolean remote, String eventUrl, MultipartFile image) {
+}
